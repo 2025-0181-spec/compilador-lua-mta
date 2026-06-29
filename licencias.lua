371bbe17c109fd3cd4fce6297b274159c52b1f51
2025-0181-spec <2025-0181@unad.edu.do>
2026-06-28 19:14:14 -0800

    Actualizar licencias

--- licencias.lua
+++ licencias.lua
@@ -1,4 +1,5 @@
 -- Archivo de licencias (modo remoto). Generado por el panel.
 -- IP activa lleva la CLAVE del sistema; bloqueada = false.
 
-return {}
+-- 172.93.111.28    activa     HX-PJ7K-W8SR-EGGN  Maguana
+return {["172.93.111.28"]="SYS-969C7CCCE374AD77EE97"}
